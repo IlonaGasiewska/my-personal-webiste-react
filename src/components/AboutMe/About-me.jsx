c437e31fbe31e3593307e2d51fb0d957496defff
Ilona Gąsiewska <i.gasiewska.ilona@gmail.com>
2024-01-14 19:01:52 +0100

    skills map added

--- src/components/AboutMe/About-me.jsx
+++ src/components/AboutMe/About-me.jsx
@@ -1,5 +1,6 @@
 import './About-me.css';
 import myPhoto2 from "../../images/Me-cut.png"
+import { skills } from '../../consts/skills';
 
 function AboutMe() {
 
@@ -16,13 +17,7 @@ function AboutMe() {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         </article>
         <ul className="skills-list">
-          <li><i class="fa-brands fa-angular"></i></li>
-          <li><i class="fa-brands fa-react"></i></li>
-          <li><i class="fa-brands fa-html5"></i></li>
-          <li><i class="fa-brands fa-css3-alt"></i></li>
-          <li><i class="fa-brands fa-github"></i></li>
-          <li><i class="fa-brands fa-bootstrap"></i></li>
-          <li><i class="fa-brands fa-js"></i></li>
+          {skills.map(skill => <i class={skill}></i>)}
         </ul>
       </div>
     </div>
