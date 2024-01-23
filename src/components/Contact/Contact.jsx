0c0dd30a452f2fe9806d06a2c85d86d7d549b47c
Ilona Gąsiewska <i.gasiewska.ilona@gmail.com>
2024-01-23 09:01:37 +0100

    contact form styles changed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,9 +11,7 @@ function Contact() {
         <textarea name="message" id="mesage" cols="30" rows="10" placeholder="Your message"></textarea>
         <input type="button" value="Send message" />
       </form>
-      <div className="form-right-side">
       <h1>Let's stay in touch</h1>
-      </div>
     </div>
   );
 }
